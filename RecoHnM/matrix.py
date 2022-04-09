8c4508734c508adbbc92309db3e2e25bdc537d68
Celine Fernandes <cel_fernandes@yahoo.fr>
2022-04-10 01:04:44 +0200

    code fini à 80%

--- RecoHnM/matrix.py
+++ RecoHnM/matrix.py
@@ -1,4 +1,3 @@
-from doctest import DocFileSuite
 from baseline0 import *
 from data import *
 import pandas as pd
@@ -13,40 +12,72 @@ def encode_categories_articles(clusters_articles_df):  #df doit avoir les champs
     cat_names=ohe.categories_
     return pd.concat([articles_hetm[['article_id']],cat_encoded], axis=1)
 
-def build_segment_base(segment):
+def build_segment_base(segment): # ce df contient toutes les transactions des individus du segment
     return segment[['customer_id']].merge(transact_history)
 
-
-def build_matrix(segment_base,X_week,horizon):
+def build_segment_horizon_transactions(segment_base,X_week,horizon):
     #filtrage sur l'horizon temporel pour connaitre les préférences des customers
     week_min= X_week-horizon+1
     week_max= X_week
     filtre=np.logical_and(segment_base['t_dat'] >= week_min,segment_base['t_dat'] <= week_max)
-
-    Xh_week_achats=segment_base[filtre][['customer_id','article_id']]
+    Xh_week_achats=segment_base[filtre][['customer_id','t_dat','article_id']]
     Xh_week_achats['counter']=1
-    Xh_week_achats=Xh_week_achats.merge(articles_categories)
+    return Xh_week_achats
 
-    #je fais un groupby par clients, sans le sort car j'ai ensuite besoin de refaire un tri par valeur de in104 derrière et j'ai
-    # a nouveau besoin de refaire le tri par client pour cela donc gain de temps de calcul car on suppose que sort_values est plus rapide que le
-    # sort de groupby
-    achats_gb_cust_cat=Xh_week_achats.groupby(['customer_id'],as_index=False,sort=False)
+def build_matrix(Xh_week_achats):
+    achats_for_matrix=Xh_week_achats.drop(columns='t_dat')
+    #achats_for_matrix['counter']=1 je pense pouvoir supprimer
+    achats_for_matrix=achats_for_matrix.merge(articles_categories)
+
+    # je fais un groupby par clients, sans le sort car j'ai ensuite besoin de refaire un tri
+    # donc gain de temps de calcul car on suppose que sort_values est plus rapide que le sort de groupby
+    achats_gb_cust_cat=achats_for_matrix.groupby(['customer_id'],as_index=False,sort=False)
     achats_gb_cust_cat_srt=achats_gb_cust_cat.sum().sort_values(by=['customer_id','counter'],ascending=False)
 
     # je refais ensuite un groupby par client pour calculer la somme de chaque dummy de categories
     segment_matrix=achats_gb_cust_cat_srt.drop(columns=['article_id']).groupby(['customer_id'],as_index=False).sum()
     return segment_matrix
 
-def get_cat_max(segment_matrix, diversity, nb_cat):
-    # diversity est compris entre 1 et 12 au maximum puisque nous devons restituer 12 articles_id comme prédiction
-    # idéalement maximum 4 pour chercher ensuite le Top des articles vendus dans cheaque catégorie considérée
-    # Si 1 on s'intéresse uniquement à la catégorie la plus férquemment acehtée sur l'horizon définit au préalable,
-    # 2 ce sera les 2 les plus importantes, etc...
+def get_max_cats(Xh_week_achats,clusters_articles_df):
+    # contrairement à la construction de la matrice, ici je veut les infos de transactions en lignes
+    # diversity sera le nbre de catégories utilisées pour proposer 12 références
+    tmp=Xh_week_achats.drop(columns='t_dat').merge(clusters_articles_df)
+    cluster_name=clusters_articles_df.columns[1]
+    achats_for_max_cats=tmp.groupby(['customer_id',cluster_name],as_index=False).sum()
+    achats_for_max_cats.sort_values(by=['customer_id','counter'],ascending=False,inplace=True)
+    achats_for_max_cats['rank']=achats_for_max_cats.groupby('customer_id')['counter'].rank(method='first',ascending=False)
+    return achats_for_max_cats
 
-    if diversity==1:
-        df=segment_matrix.iloc[:,0:-1]
-        cat_max1=DocFileSuite.max(axis=1) # je cherche le max sur les lignes
-    return df,cat_max1
+def top12_per_cat(segment_base,X_week,p,clusters_articles_df):
+    # p est le nombre de périodes utilisées pour déterminer le top12 (différent de horizon)
+    week_min= X_week-p+1
+    week_max= X_week
+    filtre=np.logical_and(segment_base['t_dat'] >= week_min,segment_base['t_dat'] <= week_max)
+    top12_cat=segment_base[filtre].drop(columns=['customer_id','t_dat']).merge(clusters_articles_df)
+    cluster_name=clusters_articles_df.columns[1]
+    top12_cat['counter']=1
+    top12_cat=top12_cat.groupby([cluster_name,'article_id'],as_index=False).sum()
+    top12_cat.sort_values(by=[cluster_name,'counter'],ascending=False,inplace=True)
+    top12_cat['rank']=top12_cat.groupby(cluster_name)['counter'].rank(method='first',ascending=False)
+    keep_first_12=top12_cat['rank']<=12
+    top12_cat_12=top12_cat[keep_first_12]
+    return top12_cat_12
+
+def top12_split(top12_cat_12,diversity): # dans cette version l'algo gère diversity in [1,2,3,4,6,12]
+        if diversity==1:
+            return top12_cat_12
+        else:
+            top_nb=12/diversity
+            return top12_cat_12[top12_cat_12['rank']<=top_nb]
+
+
+def predict()
+
+    # je donne baseline0 à tout le segment, puis j'affine ?
+    #ou pour optimiser if vide -> je donne baseline0
+
+
+
 
     #définition du Top12 par catégorie: à la valeur de garment j'associe la valeur TOP12 associée
     #calcul du Top12 par catégorie stocké sous forme de dictionnaire
