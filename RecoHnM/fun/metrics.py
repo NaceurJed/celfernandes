a6503de19de704931ee289c45f3d99f3f3983815
Mehdi Zaidi <pro.zaidi@protonmail.com>
2022-04-12 17:54:02 +0200

    correction .py

--- RecoHnM/fun/metrics.py
+++ RecoHnM/fun/metrics.py
@@ -1,81 +1,43 @@
 
 # Two main metrics are implemented:
 
-#1) Look at the list of prediction as a set, thus order does not matter, and 
+#1) Look at the list of prediction as a set, thus order does not matter when you compare the prediction with the actual purchases.
 #2) The metric that is proposed in the Kaggle competition, that is, it takes into account the order etc.
 # For more details, see https://www.kaggle.com/code/nandeshwar/mean-average-precision-map-k-metric-explained-code/notebook
 
 import numpy as np
+
+import collections
+
+def convert_dico_actual_predicted(dict_actual,dict_pred):
+    
+    #First, reduce the dictionnaries to the keys, values for which the keys are in both dicts
+    dict_actual_filtered = {key:dict_actual[key] for key in dict_actual if key in dict_pred }
+    dict_pred_filtered = {key:dict_pred[key] for key in dict_pred if key in dict_actual }
+
+    #Now, order the dictionnaries accordingly to the keys
+    dict_actual_filtered_ordered = dict(collections.OrderedDict(sorted(dict_actual_filtered.items())))
+    dict_pred_filtered_ordered = dict(collections.OrderedDict(sorted(dict_pred_filtered.items())))
+
+    #Since they are well ordrered and contain the same keys, we only need to convert into lists.
+    actual= [dict_actual_filtered_ordered.get(key,0) for key in dict_actual_filtered_ordered]
+    pred = [dict_pred_filtered_ordered.get(key,0) for key in dict_pred_filtered_ordered]
+    return actual, pred
+
 #1)
-
-def metric_no_order(actual, predicted, k=12):
-    n = len(actual)
-    set_actual = set(actual)
-    set_pred = set(predicted)
-
-    return len(set_actual - set_predicted ) / n
-
-def metric_no_order_global(actual, predicted, k=12):
-    return np.mean([metric_no_order(a,p,k) for a,p in zip(actual, predicted)])
-
-#2)
-
-
-def apk(actual, predicted, k=12):
-    """
-    Computes the average precision at k.
-    This function computes the average prescision at k between two lists of
-    items.
-    Parameters
-    ----------
-    actual : list
-             A list of elements that are to be predicted (order doesn't matter)
-    predicted : list
-                A list of predicted elements (order does matter)
-    k : int, optional
-        The maximum number of predicted elements
-    Returns
-    -------
-    score : double
-            The average precision at k over the input lists
-    """
-    if not actual:
+#Compute the metric for two arrays of items: predicted and actual. This gives the ratio of correct predictions: the closer to 1, the better.
+def metric_no_order(array_items_actual,array_items_pred):
+    n = len(array_items_actual)
+    if n == 0:
         return 0.0
+    set_actual = set(array_items_actual)
+    set_pred = set(array_items_pred)
+    return 1- len(set_actual - set_pred) / n
 
-    if len(predicted)>k:
-        predicted = predicted[:k]
+#metric applied to the full dictionnaries
+def metric_no_order_global(dict_actual,dict_pred, k=12):
+    actual, pred = convert_dico_actual_predicted(dict_actual,dict_pred)
+    n = len(actual)
+    return np.mean([metric_no_order(actual[i],pred[i]) for i in range(n)])
 
-    score = 0.0
-    num_hits = 0.0
-
-    for i,p in enumerate(predicted):
-        # first condition checks whether it is valid prediction
-        # second condition checks if prediction is not repeated
-        if p in actual and p not in predicted[:i]:
-            num_hits += 1.0
-            score += num_hits / (i+1.0)
-
-    return score / min(len(actual), k)
-
-def mapk(actual, predicted, k=12):
-    """
-    Computes the mean average precision at k.
-    This function computes the mean average prescision at k between two lists
-    of lists of items.
-    Parameters
-    ----------
-    actual : list
-             A list of lists of elements that are to be predicted 
-             (order doesn't matter in the lists)
-    predicted : list
-                A list of lists of predicted elements
-                (order matters in the lists)
-    k : int, optional
-        The maximum number of predicted elements
-    Returns
-    -------
-    score : double
-            The mean average precision at k over the input lists
-    """
-    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
 
